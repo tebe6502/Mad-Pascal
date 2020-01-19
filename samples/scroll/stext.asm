d79b1097b62fe7d2269eba7bfa1c87c4bc0b3653
Tomasz Biela <tebe6502@o2.pl>
2020-01-20 00:58:02 +0100

    Add files via upload

---
+++ samples/scroll/stext.asm
@@ -0,0 +1,7 @@
+
+	dta d'some SCROLLING '
+	dta d'will OCCUR '*
+	dta d'No',d'W'*,d'. no big tricks, but using PASCAL CODE within dli and vbl '
+	dta d'seems TO COST a lot OF CPU time. '*
+	dta d'so you HAVE to be ',d'cArE'*,d'fUl with code in ',d'VBL or DLI'*,d'.....                '
+	dta $ff
