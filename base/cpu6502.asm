b429be5b90bd93ff79f018a3a12c057622a16f05
Tomasz Biela <tebe6502@o2.pl>
2020-01-24 00:33:22 +0100

    Add files via upload

--- base/cpu6502.asm
+++ base/cpu6502.asm
@@ -632,117 +632,6 @@ _lo	sta :STACKORIGIN-1+STACKWIDTH*3,x
 	rts
 .endp
 
-/*
-.proc	@cmpFor_WORD (.word ya) .reg
-	sta ztmp
-	sty ztmp+1
-
-	ldy #1
-	lda (ztmp),y
-	cmp :STACKORIGIN+1+STACKWIDTH,x
-	bne stop
-	dey
-	lda (ztmp),y
-	cmp :STACKORIGIN+1,x
-stop	rts
-.endp
-
-
-.proc	@cmpFor_CARD (.word ya) .reg
-	sta ztmp
-	sty ztmp+1
-
-	ldy #3
-	lda (ztmp),y
-	cmp :STACKORIGIN+1+STACKWIDTH*3,x
-	bne stop
-	dey
-	lda (ztmp),y
-	cmp :STACKORIGIN+1+STACKWIDTH*2,x
-	bne stop
-	dey
-	lda (ztmp),y
-	cmp :STACKORIGIN+1+STACKWIDTH,x
-	bne stop
-	dey
-	lda (ztmp),y
-	cmp :STACKORIGIN+1,x
-
-stop	rts
-.endp
-
-
-.proc	@cmpFor_SHORTINT(.word ya) .reg
-	sta ztmp
-	sty ztmp+1
-
-	ldy	#0
-	lda	(ztmp),y
-	sec
-	sbc	:STACKORIGIN+1,x
-	bne	@cmpFor_INT.L4
-
-	jmp	@cmpFor_INT.L1
-.endp
-
-
-.proc	@cmpFor_SMALLINT(.word ya) .reg
-	sta ztmp
-	sty ztmp+1
-
-	ldy	#1
-	lda	(ztmp),y
-	sec
-	sbc	:STACKORIGIN+1+STACKWIDTH,x
-	bne	@cmpFor_INT.L4
-
-	dey
-	lda	(ztmp),y
-	cmp	:STACKORIGIN+1,x
-
-	jmp	@cmpFor_INT.L1
-.endp
-
-
-.proc	@cmpFor_INT(.word ya) .reg
-	sta ztmp
-	sty ztmp+1
-
-	ldy	#3
-	lda	(ztmp),y
-	sec
-	sbc	:STACKORIGIN+1+STACKWIDTH*3,x
-	bne	L4
-
-	dey
-	lda	(ztmp),y
-	cmp	:STACKORIGIN+1+STACKWIDTH*2,x
-	bne	L1
-
-	dey
-	lda	(ztmp),y
-	cmp	:STACKORIGIN+1+STACKWIDTH,x
-	bne	L1
-
-	dey
-	lda	(ztmp),y
-	cmp	:STACKORIGIN+1,x
-
-L1	beq	L2
-	bcs	L3
-
-	lda	#$FF	; Set the N flag
-L2	rts
-
-L3	lda	#$01	; Clear the N flag
-	rts
-
-L4	bvc	L5
-	eor	#$FF	; Fix the N flag if overflow
-	ora	#$01	; Clear the Z flag
-L5	rts
-.endp
-*/
 
 ; Piotr Fusik, 15.04.2002
 ; originally by Ullrich von Bassewitz
@@ -751,9 +640,13 @@ L5	rts
 	lda	:STACKORIGIN-1,x
 	clv:sec
 	sbc	:STACKORIGIN,x
-	bne	cmpINT.L4
+	bne     @L4
+@L3	rts
 
-	jmp	cmpINT.L1
+@L4:    bvc     @L3
+	eor     #$FF		; Fix the N flag if overflow
+	ora     #$01		; Clear the Z flag
+	rts	
 .endp
 
 
@@ -761,12 +654,21 @@ L5	rts
 	lda	:STACKORIGIN-1+STACKWIDTH,x
 	clv:sec
 	sbc	:STACKORIGIN+STACKWIDTH,x
-	bne	cmpINT.L4
+	bne     @L4
 
 	lda	:STACKORIGIN-1,x
-	cmp	:STACKORIGIN,x
+	cmp	:STACKORIGIN,x	; Compare low byte
+	beq     @L3
 
-	jmp	cmpINT.L1
+	lda	#$00
+	adc     #$FF		; If the C flag is set then clear the N flag
+	ora     #$01		; else set the N flag
+@L3:    rts
+
+@L4:    bvc     @L3
+	eor     #$FF		; Fix the N flag if overflow
+	ora     #$01		; Clear the Z flag
+	rts	
 .endp
 
 
