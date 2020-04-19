0994ef0b62ba98db069927464d1be4a9d4005978
Tomasz Biela <tebe6502@o2.pl>
2020-04-19 17:59:30 +0200

    Add files via upload

--- lib/s2.pas
+++ lib/s2.pas
@@ -22,12 +22,60 @@ interface
 	procedure SetGraphMode(mode: byte);
 	procedure ClearDevice;
 	procedure CloseGraph;
+{	procedure Print(a: cardinal); overload;
+	procedure Print(a: integer); overload;
 	procedure Print(a: char); overload;
 	procedure Print(a: PString); overload;
-
+}
 implementation
 
-uses cio, graph, crt;
+uses cio, graph, crt, sysutils;
+
+
+procedure ClearDevice;
+begin
+
+ cls(6);
+ opn(6,12,GraphMode,'S2:');
+
+end;
+
+
+(*
+procedure s2_put;
+begin
+
+asm
+{
+	pha
+	tya
+	pha
+
+	stx @sp
+
+	cmp #eol
+	beq skp
+
+	cmp #$7d		; clrscr
+	bne skp
+
+	ldx @sp			; restore X !
+};
+
+	ClearDevice;
+asm
+{
+skp
+	pla
+	tay
+	pla
+
+	ldx #0
+@sp	equ *-1
+};
+
+end;
+*)
 
 
 procedure SetGraphMode(mode: byte);
@@ -35,6 +83,7 @@ procedure SetGraphMode(mode: byte);
 @description:
 Init S2:
 *)
+var p: pointer;
 begin
 
  InitGraph(0);
@@ -42,38 +91,32 @@ begin
  cls(6);
  opn(6,12,mode,'S2:');
  
- GraphResult:=IOResult;
- 
- GraphMode := mode;
- 
 asm
 {
 	txa:pha
 
-	ldx #$60
+	ldy icax5+$60	; vertical resolution Y
+	bne ok
 
-	ldy icax5,x	; vertical resolution Y
+	lda #MAIN.GRAPH.grNotDetected
+	bne toEnd
 
-	lda icax3,x	; horizontal resolution X:A
-	pha
-	lda icax4,x
-	tax
-
-	pla
+ok	lda icax3+$60	; horizontal resolution X:A
+	ldx icax4+$60
 
 	sta MAIN.SYSTEM.ScreenWidth
 	stx MAIN.SYSTEM.ScreenWidth+1
-	
+
 	sub #1
 	sta MAIN.GRAPH.WIN_RIGHT
 	txa
 	sbc #0
 	sta MAIN.GRAPH.WIN_RIGHT+1
-	
+
 	sty MAIN.SYSTEM.ScreenHeight
 	lda #0
 	sta MAIN.SYSTEM.ScreenHeight+1
-	
+
 	sta MAIN.GRAPH.WIN_LEFT
 	sta MAIN.GRAPH.WIN_LEFT+1
 	sta MAIN.GRAPH.WIN_TOP
@@ -82,12 +125,24 @@ asm
 	sta MAIN.GRAPH.WIN_BOTTOM+1	
 	dey
 	sty MAIN.GRAPH.WIN_BOTTOM
+
+	lda mode
+	sta MAIN.SYSTEM.GraphMode
+
+  	mva #$2c @putchar.vbxe	; bit*
+	mva #$60 @putchar.chn	; #6
+	mva #0 766		; execution execution control character
 	
+	lda #MAIN.GRAPH.grOK
+toEnd
+	sta MAIN.GRAPH.GraphResult
+
 	pla:tax
 };
 end;
 
 
+{
 procedure Print(a: char); overload;
 begin
 
@@ -105,13 +160,17 @@ begin
 end;
 
 
-procedure ClearDevice;
+procedure Print(a: cardinal); overload;
 begin
-
- cls(6);
- opn(6,12,GraphMode,'S2:');
-
+ Print( IntToStr(a) );
 end;
+
+
+procedure Print(a: integer); overload;
+begin
+ Print( IntToStr(a) );
+end;
+}
 
 
 procedure CloseGraph;
@@ -120,6 +179,38 @@ begin
  cls(6);
 
  TextMode(0);
+ 
+end;
+
+
+
+initialization
+
+
+SetGraphMode(0);
+
+if GraphResult <> grOK then begin
+
+ xio(40,1,0,0,'D:SDXLD.COM');
+ 
+ if IoResult >= 128 then begin
+  TextMode(0);
+  writeln('S_VBXE.SYS not installed');
+
+  repeat until keypressed;
+  halt;
+ end;
+
+ SetGraphMode(0);
+ 
+ if GraphResult <> grOK then begin
+  TextMode(0);
+  writeln('VBXE not present');
+ 
+  repeat until keypressed;
+  halt;
+ end;
+   
 end;
 
 
