6db2c942ba1141a2319945564b62888661872176
Tomasz Biela <tebe6502@o2.pl>
2020-05-10 19:58:59 +0200

    Add files via upload

--- lib/s2.pas
+++ lib/s2.pas
@@ -26,10 +26,17 @@ unit S2;
 {
 
 ClearDevice
+ClearLine
 CloseGraph
+GetPaletteEntries
 InitGraph
+LoadBitmap
 Position
+SaveBitmap
+ScrollDown
+ScrollUp
 SetGraphMode
+SetPaletteEntries
 TextOut
 
 }
@@ -46,18 +53,20 @@ const	fsNormal = 0;
 var	Font: Object
 			Style: byte;
 			Color: byte;
-
-//			procedure LoadFromFile(fname: PByte);
 	end;
 
 
 	procedure SetGraphMode(mode: byte);
 	procedure ClearDevice;
+	procedure ClearLine(line, cnt: byte);
 	procedure CloseGraph;
+	procedure GetPaletteEntries(pal: pointer);
 	procedure Position(x: word; y: byte); assembler;
 	procedure ScrollUp(line, cnt: byte);
 	procedure ScrollDown(line, cnt: byte);
-	procedure ClearLine(line, cnt: byte);
+	procedure SetPaletteEntries(pal: pointer);
+	function LoadBitmap(fnam: PString): Boolean;
+	procedure SaveBitmap(fnam: PString);
 	procedure TextOut(x: word; y: byte; s: PByte); overload;
 	procedure TextOut(a: char); overload;
 
@@ -67,6 +76,15 @@ implementation
 uses cio, crt, graph, sysutils;
 
 {$i imageh.inc}
+
+var
+	buffer: array [0..0] of byte absolute $0400;
+	tmp: array [0..3] of byte;
+
+	Header: TBmpHeader;
+	f: file;
+
+
 
 procedure ClearDevice;
 (*
@@ -155,7 +173,7 @@ procedure Position(x: word; y: byte); assembler;
 @description:
 Set cursor position on screen.
 
-GotoXY positions the cursor at (X,Y), X in horizontal, Y in vertical direction.
+Positions the cursor at (X,Y), X in horizontal, Y in vertical direction.
 
 @param: x - horizontal positions
 @param: y - vertical positions
@@ -259,6 +277,173 @@ begin
  cls(6);
 
  TextMode(0);
+
+end;
+
+
+procedure GetPaletteEntries(pal: pointer);
+begin
+
+ xio(102,6,64+12,hi(word(pal)),'S2:');
+
+end;
+
+
+procedure SetPaletteEntries(pal: pointer);
+begin
+
+ xio(101,6,64+12,hi(word(pal)),'S2:');
+
+end;
+
+
+procedure SaveBitmap(fnam: PString);
+var i: byte;
+begin
+
+ Header.bfType := ord('M')*256 + ord('B');
+ Header.biSize := 40;
+ Header.biWidth := ScreenWidth;
+ Header.biHeight := ScreenHeight;
+ Header.bfOffbits := 14 + Header.biSize + 1024;
+ Header.biSizeImage := Header.biWidth * Header.biHeight;
+ Header.bfSize := Header.bfOffbits + Header.biSizeImage;
+
+ Header.biClrUsed := 256;		// liczba kolorow
+ Header.biClrImportant:=0;
+ Header.biXPelsPerMeter:=0;
+ Header.biYPelsPerMeter:=0;
+ Header.bfReserved:=0;
+ Header.biCompression:=0;
+ Header.biPlanes:=1;
+ Header.biBitcount:=8;			// liczba bitow na pixel
+
+ assign(f, fnam); rewrite(f, 1);
+ blockwrite(f, Header, sizeof(TBMPHeader));
+
+ GetPaletteEntries(Buffer);
+
+ for i:=0 to 255 do begin
+  tmp[0]:=Buffer[$200+i];
+  tmp[1]:=Buffer[$100+i];
+  tmp[2]:=Buffer[i];
+  tmp[3]:=0;
+
+  blockwrite(f, tmp, 4);
+ end;
+
+
+ for i:=191 downto 0 do begin
+ 
+  Position(0,i);
+
+  BGet(6, Buffer, 320);
+  blockwrite(f, Buffer, 320);
+
+ end;
+
+ close(f);
+
+end;
+
+
+function LoadBitmap(fnam: PString): Boolean;
+(*
+@description:
+This loads a BMP File (4bit, 8bit)
+*)
+var x, w, h: word;
+    b, v, i: byte;
+    a: cardinal;
+begin
+
+ if not FileExists(fnam) then begin
+//   IMGError:=FileNotFound;
+   Result:=false;
+   exit;
+ end;
+
+ assign(f, fnam); reset(f, 1);
+
+ blockread(f, Header, sizeof(Header));
+
+ w:=Header.biwidth;
+ h:=Header.biheight;
+ b:=Header.bibitcount;
+
+ {Check to see whether we can display it}
+ if (Header.bfType <> 19778)
+ or (Header.bfReserved <> 0)
+ or (Header.biPlanes <> 1)
+ or (Header.biCompression <> 0)
+ or (b <> 8) then begin
+   Close (f);
+//   IMGError := UnsupportedFormat;
+   Result := false;
+   Exit;
+ end;
+
+ if (w>320) or (h>192) then begin
+//  IMGError := TooLarge;
+  Result := false;
+  Exit;
+ end;
+
+ if w and 3<>0 then w:=w and $fffc + 4;
+
+ dec(h);
+
+ blockread(f, Buffer, Header.bfOffBits-1024-sizeof(TBMPHeader));	// offset do tablicy pikseli obrazka
+
+ {Set the palette}
+
+ for i:=0 to 255 do begin
+  blockread(f, tmp, 4);
+
+  Buffer[i+$200]:= tmp[0];
+  Buffer[i+$100]:= tmp[1];
+  Buffer[i]	:= tmp[2];
+ end;  
+  
+ SetPaletteEntries(Buffer);
+
+
+ x:=0;
+
+ Position(0,h);
+
+ while not eof(f) do begin
+
+	blockread(f, Buffer, 256);
+
+	if word(x+256) < w then begin
+	
+	 BPut(6, Buffer, 256);
+	 inc(x, 256);
+
+	end else
+
+	for i:=0 to 255 do begin
+
+	 Put(6, Buffer[i]);
+
+	 inc(x);
+
+	 if x = w then begin
+		x:=0;
+		dec(h);
+		Position(0,h);
+	 end;
+
+	end;
+
+ end;
+
+ {Close the file}
+ Close (f);
+
+ {Successful}
+ Result := true;
 
 end;
 
