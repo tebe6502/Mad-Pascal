6cb4a4076be282d7898e24ce15b6d318f9800a48
Tomasz Biela <tebe6502@o2.pl>
2020-05-31 14:15:03 +0200

    Add files via upload

--- samples/compression/unapl_stream.pas
+++ samples/compression/unapl_stream.pas
@@ -1,15 +1,10 @@
 uses crt, aplib, graph;
 
-{$r unapl.rc}
-
-const
-	aplpic = $9000;
-
 begin
 
  InitGraph(15+16);
 
- unapl(pointer(aplpic), pointer(dpeek(88)) );
+ unAPL('D:KORONIS.APL', pointer(dpeek(88)) );
 
  repeat until keypressed;
 
