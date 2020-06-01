e712c60ac087dc1b82f10be277a53da05bb154b5
Tomasz Biela <tebe6502@o2.pl>
2020-06-02 01:48:58 +0200

    Add files via upload

--- mp.pas
+++ mp.pas
@@ -1176,7 +1176,7 @@ var TempIndex: integer;
     Result := 0;
 
     for BlockStackIndex := BlockStackTop downto 0 do       // search all nesting levels from the current one to the most outer one
-    for IdentIndex := 1 to NumIdent do
+    for IdentIndex := NumIdent downto 1 do
       if (X = Ident[IdentIndex].Name) and (BlockStack[BlockStackIndex] = Ident[IdentIndex].Block) then
 	if (Ident[IdentIndex].UnitIndex = UnitIndex) {or Ident[IdentIndex].Section} or (Ident[IdentIndex].UnitIndex = 1) or (UnitName[Ident[IdentIndex].UnitIndex].Name = 'SYSTEM') or UnitAllowedAccess(IdentIndex, UnitIndex) then begin
 	  Result := IdentIndex;
@@ -1197,7 +1197,7 @@ var TempIndex: integer;
     Result := 0;
 
     for BlockStackIndex := BlockStackTop downto 0 do       // search all nesting levels from the current one to the most outer one
-    for IdentIndex := 1 to NumIdent do
+    for IdentIndex := NumIdent downto 1 do
       if (X = Ident[IdentIndex].Name) and (BlockStack[BlockStackIndex] = Ident[IdentIndex].Block) then
 	if (Ident[IdentIndex].UnitIndex = UnitIndex) or UnitAllowedAccess(IdentIndex, UnitIndex) then begin
 	  Result := IdentIndex;
