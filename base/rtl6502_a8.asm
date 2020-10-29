2f9ff8bc427531c1bd539bb690bec692b548bbee
Tomasz Biela <tebe6502@o2.pl>
2020-10-30 00:29:39 +0100

    Add files via upload

--- base/rtl6502_a8.asm
+++ base/rtl6502_a8.asm
@@ -196,64 +196,7 @@ m@call	.macro (os_proc)
 	icl 'common\printsingle.asm'	; @FTOA
 	icl 'common\printfloat.asm'	; @printSHORTREAL, @printREAL, @float
 
-; -----------------------------------------------------------------------
-
-
-.proc	@AllocMem	;(.word ztmp .byte ztmp+2) .var
-
-	sta ztmp+1
-	sty ztmp+2
-
-loop	lda (psptr),y
-	sta ztmp+3
-
-	lda (ztmp),y
-	sta (psptr),y
-
-	lda ztmp+3
-	sta (ztmp),y
-
-	dey
-	bpl loop
-
-	lda psptr
-	sec
-	adc ztmp+2
-	sta psptr
-	scc
-	inc psptr+1
-
-	rts
-.endp
-
-
-.proc	@FreeMem	;(.word ztmp .byte ztmp+2) .var
-
-	sta ztmp+1
-
-	tya
-	eor #$ff
-	clc
-	adc psptr
-	sta psptr
-	scs
-	dec psptr+1
-
-loop	lda (psptr),y
-	sta ztmp+3
-
-	lda (ztmp),y
-	sta (psptr),y
-
-	lda ztmp+3
-	sta (ztmp),y
-
-	dey
-	bpl loop
-
-	rts
-.endp
-
+	icl 'common\allocmem.asm'	; @AllocMem, @FreeMem
 
 ; -----------------------------------------------------------------------
 
