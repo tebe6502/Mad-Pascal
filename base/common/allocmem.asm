a8623ee9a65d0eb4249013ef430a5e2a7a487e38
Tomasz Biela <tebe6502@o2.pl>
2020-10-30 00:30:33 +0100

    Add files via upload

---
+++ base/common/allocmem.asm
@@ -0,0 +1,55 @@
+
+.proc	@AllocMem	;(.word ztmp .byte ztmp+2) .var
+
+	sta ztmp+1
+	sty ztmp+2
+
+loop	lda (psptr),y
+	sta ztmp+3
+
+	lda (ztmp),y
+	sta (psptr),y
+
+	lda ztmp+3
+	sta (ztmp),y
+
+	dey
+	bpl loop
+
+	lda psptr
+	sec
+	adc ztmp+2
+	sta psptr
+	scc
+	inc psptr+1
+
+	rts
+.endp
+
+
+.proc	@FreeMem	;(.word ztmp .byte ztmp+2) .var
+
+	sta ztmp+1
+
+	tya
+	eor #$ff
+	clc
+	adc psptr
+	sta psptr
+	scs
+	dec psptr+1
+
+loop	lda (psptr),y
+	sta ztmp+3
+
+	lda (ztmp),y
+	sta (psptr),y
+
+	lda ztmp+3
+	sta (ztmp),y
+
+	dey
+	bpl loop
+
+	rts
+.endp
