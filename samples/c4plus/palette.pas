492823b19f659ad466934a7824c8f0e0a024b31f
zbyti <zbyti@yahoo.com>
2021-01-12 12:10:21 +0100

    refactor

--- samples/c4plus/palette.pas
+++ samples/c4plus/palette.pas
@@ -10,12 +10,10 @@ var
 
 begin
   BORDERCOLOR := 0; BGCOLOR := 0;
-
-  for w0i := 0 to (40 * 25 - 1) do SCREEN[w0i] := $a0;
+  FillByte(@SCREEN, 40 * 25, $a0);
 
   for b0i := 0 to 7 do begin
     row := 40 * b0i;
     for b1i := 0 to 15 do COLORRAM[row+b1i] := (b0i shl 4) + b1i;
   end;
-
 end.
