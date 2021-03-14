6b7204b484c3a300c93db3ea327f54d77cf16d6b
Tomasz Biela <tebe6502@o2.pl>
2021-03-14 20:24:06 +0100

    c64, c4plus $resource

---
+++ base/c64/resource.asm
@@ -0,0 +1,50 @@
+
+	opt l-
+
+/*
+	RCASM
+	RCDATA
+*/
+
+
+/* ----------------------------------------------------------------------- */
+/* RCASM
+/* ----------------------------------------------------------------------- */
+
+.macro	RCASM (nam, lab)
+
+	ift main.%%lab>=CODEORIGIN && main.%%lab<PROGRAMSTACK
+	ert 'Overlap memory'
+	eif
+
+	org main.%%lab
+len_
+
+	icl %%1
+
+len = * - len_
+
+	.print '$R RCASM   ',main.%%lab,'..',main.%%lab+len-1," %%1"
+.endm
+
+
+/* ----------------------------------------------------------------------- */
+/* RCDATA
+/* ----------------------------------------------------------------------- */
+
+.macro	RCDATA (nam, lab)
+
+len = .filesize(%%1)
+
+	ift main.%%lab>=CODEORIGIN && main.%%lab<PROGRAMSTACK
+	ert 'Overlap memory'
+	eif
+
+	org main.%%lab
+
+	ins %%1
+
+	.print '$R RCDATA  ',main.%%lab,'..',*-1," %%1"
+.endm
+
+	opt l+
