b8290ce573054ca878a1559c9655f5807de44727
zbyti <zbyti@yahoo.com>
2021-03-17 22:10:20 +0100

    refactor

--- samples/c4plus/TEDzakker/music.pas
+++ samples/c4plus/TEDzakker/music.pas
@@ -5,16 +5,11 @@
 const
   MUSIC = $4000;
 
-var
-  VCOUNT                             : byte absolute $ff1d;  //vertical line bits 0-7
-
 begin
-  pause;
-
   asm { phr \ jsr MUSIC \ plr };
 
   repeat
-    repeat until VCOUNT = $d8;
+    pause;
     asm { phr \ lda #0 \ jsr MUSIC+3 \ plr };
   until false;
 end.
