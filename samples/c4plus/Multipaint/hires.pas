e6ba6488e8e788a8ceea2950c092b6fa08737a6e
zbyti <zbyti@yahoo.com>
2021-03-20 11:17:17 +0100

    Mad Pascal logo and hires gfx example

--- samples/c4plus/Multipaint/hires.pas
+++ samples/c4plus/Multipaint/hires.pas
@@ -1,31 +1,26 @@
-{$r gfx.rc}
-
-// sample gfx made by Carrion
+//Mad Pascal logo by @bocianu
+{$r hgfx.rc}
 
 const
-  TRON_TITLE_SCREEN = $5800;
+  LOGO = $5800;
 
 var
   SETBITMAP                          : byte absolute $ff06;
   SETMCOLOR                          : byte absolute $ff07;
   BITMAPADDR                         : byte absolute $ff12;
   VIDEOMATRIX                        : byte absolute $ff14;
-  BACKGROUND                         : byte absolute $ff15;
-  COLOUR1                            : byte absolute $ff16;
   BORDER                             : byte absolute $ff19;
 
 begin
   SETBITMAP := SETBITMAP or $20;
-  SETMCOLOR := (SETMCOLOR and $40) or $18;
+  SETMCOLOR := (SETMCOLOR and $40) or $8;
 
   // (01011xxx) $5800 = 11 * $800;
   VIDEOMATRIX := %01011000;
   // (xx011xxx) $6000 = 3 * $2000; bit 2 set to 0 means reading from RAM
   BITMAPADDR := %00011000 or (BITMAPADDR and %00000011);
 
-  BORDER := 0;
-  BACKGROUND := 0;
-  COLOUR1 := 1;
+  BORDER := $3d;
 
   repeat until false;
 end.
