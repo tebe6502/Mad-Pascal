0a924e725cedd06bbd71e99778580e94e51736b6
Tomasz Biela <tebe6502@o2.pl>
2021-04-14 22:48:28 +0200

    Add files via upload

--- base/atari/vbxexdl.asm
+++ base/atari/vbxexdl.asm
@@ -24,22 +24,24 @@ idx	equ *-1
 	fxs FX_MEMS #0
 	rts
 
-msk	.array [6] .word
+msk	.array [7] .word
 	[e@xdl.mapon]  = [XDLC_MAPON|XDLC_MAPOFF]^$FFFF
 	[e@xdl.mapoff] = [XDLC_MAPON|XDLC_MAPOFF]^$FFFF
 	[e@xdl.ovron]  = [XDLC_GMON|XDLC_OVOFF|XDLC_LR|XDLC_HR]^$FFFF
 	[e@xdl.ovroff] = [XDLC_GMON|XDLC_OVOFF|XDLC_LR|XDLC_HR]^$FFFF
 	[e@xdl.hr]     = [XDLC_GMON|XDLC_OVOFF|XDLC_LR|XDLC_HR]^$FFFF
 	[e@xdl.lr]     = [XDLC_GMON|XDLC_OVOFF|XDLC_LR|XDLC_HR]^$FFFF
+	[e@xdl.tmon]   = [XDLC_GMON|XDLC_TMON|XDLC_MAPOFF]^$FFFF
 	.enda
 
-val	.array [6] .word
+val	.array [7] .word
 	[e@xdl.mapon]  = XDLC_MAPON
 	[e@xdl.mapoff] = XDLC_MAPOFF
 	[e@xdl.ovron]  = XDLC_GMON
 	[e@xdl.ovroff] = XDLC_OVOFF
 	[e@xdl.hr]     = XDLC_GMON|XDLC_HR
 	[e@xdl.lr]     = XDLC_GMON|XDLC_LR
+	[e@xdl.tmon]   = XDLC_TMON
 	.enda
 
 .endp
