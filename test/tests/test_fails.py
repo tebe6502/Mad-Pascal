ddb9eba392eb810ab5c891f4fc7feec6adfedd64
bocianu <bocianu+gitlab@gmail.com>
2021-05-26 01:30:35 +0200

    comments added, code cleanup, some improvements

--- test/tests/test_fails.py
+++ test/tests/test_fails.py
@@ -1,5 +1,5 @@
 from testutils import testUtils
-test = testUtils();
+test = testUtils()
 
 class Test_Failures:
 
@@ -10,7 +10,7 @@ class Test_Failures:
           itout  : word;
           itin   : word;
           index  : word;
-          primes : array[0..2] of word absolute $2000;
+          primes : array[0..3] of word absolute $2000;
         begin
           index := 0;
           primes[0] := 2; primes[1] := 3; inc(index, 2);
@@ -25,7 +25,28 @@ class Test_Failures:
             end;
           end;
         end.
-        """);
-        assert test.getWord(0x2000 + 2*2) == 5;
-        assert test.getWord(0x2000 + 3*2) == 7;
+        """)
+        assert test.getWord(0x2000 + 2*2) == 5
+        assert test.getWord(0x2000 + 3*2) == 7
+
+      
+    def test_consecutive_reading_of_changing_register_fails (self):
+      
+        test.runCode("""
+        var a, b, c: byte;
+            timer: byte = 0; // initialized to suppress warning
+        begin
+            a := timer;
+            asm { nop }; // nop fixes that
+            b := timer;
+                          // without fix
+            c := timer;
+        end.
+        """,
+        counters = ['timer'] )
+            
+        assert test.varByte('a') != test.varByte('b')
+        assert test.varByte('b') != test.varByte('c')
+
+        test.clearCounters()
 
