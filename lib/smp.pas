2e9d4aefd8649789b5e44aa40a72ab6d19c0f28e
Tomasz Biela <tebe6502@o2.pl>
2021-06-20 21:15:32 +0200

    unit SMP -

--- lib/smp.pas
+++ lib/smp.pas
@@ -51,17 +51,18 @@ sample	equ *-2
 
 	lda #4
 	sta IRQEN			; re-enable only timer 4
-	eor #0				; switch between 0 and 1 (right and left nybble)
+	eor #0				; switch between 0 and 4 (right and left nybble)
 nybble	equ *-1
 	sta nybble
-
 	beq irq1
+
 	tya
 	lsr @
 	lsr @
 	lsr @
 	lsr @				; left nybble of sample
 	bpl irq2			; always branch
+
 irq1	tya
 	and #$0f			; right nybble of sample
 	inc sample			; next lo byte of sample
@@ -70,18 +71,16 @@ irq1	tya
 	dec size			; check if at end of sample
 	bne irq2			; branch if not
 	lda #0
+size	equ *-1				; = 0
 	sta IRQEN			; end of sample - reset interrupt
 	beq irq3			; always branch
+
 irq2	ora #$f0			; no polycounters + volume only bit
 irq3	sta value			; save sample to play next irq
+
 	pla
 	tay
 	pla				; restore Y and A
-
-	rti
-
-size	brk
-
 end;
 
 
@@ -92,17 +91,18 @@ Play music, call this procedure every VBL frame
 *)
 asm
 	sei
-	mva #0 IRQEN
+	mvy #0 IRQEN
 
 	mwa	#MAIN.SMP.PlaySample	$fffe		; setup custom IRQ handler
 
-	mva :bp2	l_hlp+1
-	mva :bp2+1	h_hlp+1
+	lda :bp2
+	pha
+	lda :bp2+1
+	pha
 
 	mwa TSMP :bp2
 
-	ldy #0
-	lda (:bp2),y
+	lda (:bp2),y			; Y = 0
 	sta PlaySample.sample
 	iny
 	lda (:bp2),y
@@ -111,8 +111,10 @@ asm
 	lda (:bp2),y
 	sta PlaySample.size
 
-l_hlp	mva #0 :bp2
-h_hlp	mva #0 :bp2+1
+	pla
+	sta :bp2+1
+	pla
+	sta :bp2
 
 	lda #1				; 0=POKEY 64KHz, 1=15KHz
 	sta AUDCTL
@@ -124,22 +126,20 @@ h_hlp	mva #0 :bp2+1
 	lda #4
 	sta IRQEN			; enable timer 4
 
+	lda #$f8
+	sta PlaySample.value		; an initial sample value (with no polycounters + volume only bit)
 	lda #0
 	sta PlaySample.nybble		; initialize nybble
-	ldx #$f8
-	stx PlaySample.value		; an initial sample value (with no polycounters + volume only bit)
 
-	lda #$70
-wait3	cmp VCOUNT
+	ldy #$70
+wait3	cpy VCOUNT
 	bne wait3			; sync to a scanline
 
-	lda #0
-	sta SKCTL
+	sta SKCTL			; A = 0
 	lda #3
 	sta SKCTL			; test - reset pokey and polycounters
 	sta STIMER			; start timers
 	cli
-
 end;
 
 
