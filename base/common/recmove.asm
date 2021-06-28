faf8f5b402adaf9f86bca4efce9407dfe9c8933d
Tomasz Biela <tebe6502@o2.pl>
2021-06-28 23:36:47 +0200

    TEXTFILE

---
+++ base/common/recmove.asm
@@ -0,0 +1,30 @@
+
+; Record move
+
+/*
+	@moveRECORD
+	@moveSTRING_1
+*/
+
+.proc	@moveRECORD (.word ya) .reg
+
+	sta :bp2
+	sty :bp2+1
+
+	ldy #1
+lop	lda @buf-1,y
+	cmp #eol
+	beq exit
+
+	sta (:bp2),y
+
+	iny
+	bne lop
+
+exit	dey
+	tya
+	ldy #0
+	sta (:bp2),y
+
+	rts
+.endp
