13332899cb2b9edb4fd63d76c960e323dca9b3b6
Tomasz Biela <tebe6502@o2.pl>
2021-07-22 08:36:20 +0200

    Add files via upload

--- test/tests/test_mul.py
+++ test/tests/test_mul.py
@@ -2,9 +2,9 @@ from testutils import testUtils
 test = testUtils()
 
 class Test_Multiplications:
-        
+
     def test_simple_multiplication(self):
-        
+
         test.runCode('''
         procedure breaktest; begin end;
 
@@ -15,23 +15,29 @@ class Test_Multiplications:
             b:=27;
             a:=b*3;
             breaktest;
-            
+
             a:=b*5;
             breaktest;
-            
+
             a:=b*6;
             breaktest;
-            
+
             a:=b*7;
             breaktest;
-            
+
             a:=b*9;
-        end.        
+	    breaktest;
+
+	    a:=b*128;
+	    breaktest;
+
+	    a:=b*256;
+        end.
         ''', ['breaktest'])
-        
-        assert test.varByte('b') == 27;   
+
+        assert test.varByte('b') == 27;
         assert test.varWord('a') == 27 * 3
-        
+
         test.resume()
         assert test.varWord('a') == 27 * 5
 
@@ -43,7 +49,13 @@ class Test_Multiplications:
 
         test.resume()
         assert test.varWord('a') == 27 * 9
-    
+
+        test.resume()
+        assert test.varWord('a') == 27 * 128
+
+        test.resume()
+        assert test.varWord('a') == 27 * 256
+
     def test_another_multiplication_inline(self):
 
         test.runCode("""
@@ -56,6 +68,23 @@ class Test_Multiplications:
             w := a * 10;
         end.
         """)
-    
+
         assert test.varWord('w') == 23 * 45 * 10
 
+
+    def test_mul256(self):
+
+        test.runCode("""
+        var b:byte;
+            w, a:word;
+        begin
+            b := 184;
+            w := b * 256;
+
+	    a := 184;
+	    a := a * 256;
+        end.
+        """)
+
+        assert test.varWord('w') == 47104
+        assert test.varWord('a') == 47104
--- test/tests/test_predicates.py
+++ test/tests/test_predicates.py
@@ -2,19 +2,19 @@ from testutils import testUtils
 test = testUtils()
 
 class Test_Predicates:
-        
+
     def test_lteq(self):
         test.runFile('tests/sources/predicates_lteq.pas')
         assert test.varByte('i') == 10
 
     def test_ne(self):
         test.runFile('tests/sources/predicates_ne.pas')
-        assert test.varByte('i') == 12
+        assert test.varCardinal('i') == 4095
 
     def test_gt(self):
         test.runFile('tests/sources/predicates_gt.pas')
-        assert test.varByte('i') == 9
-  
+        assert test.varCardinal('i') == 767
+
     def test_gteq(self):
         test.runFile('tests/sources/predicates_gteq.pas')
         assert test.varByte('i') == 12
@@ -22,8 +22,8 @@ class Test_Predicates:
     def test_lt(self):
         test.runFile('tests/sources/predicates_lt.pas')
         assert test.varByte('i') == 7
-  
+
     def test_eq(self):
         test.runFile('tests/sources/predicates_eq.pas')
-        assert test.varByte('i') == 6
-  
+        assert test.varCardinal('i') == 258048
+
