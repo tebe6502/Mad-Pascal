52db46a4bd6cdf114c3804720cc1889c6406a97b
Tomasz Biela <tebe6502@o2.pl>
2021-12-05 01:45:06 +0100

    Add files via upload

--- base/c64/putchar.asm
+++ base/c64/putchar.asm
@@ -7,15 +7,11 @@
 
 .proc	@putchar (.byte a) .reg
 
-chrout	= $ffd2                ;kernel character output sub
+chrout	= $ffd2			;kernel character output sub
 
 	cmp #64
 	scc
 	eor #%00100000
 
-	jsr chrout
-
-;	lda #$00
-;	sta $d4
-	rts
+	jmp chrout
 .endp
