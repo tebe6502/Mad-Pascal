65c8c7f618e5d5038c36a248eb7c35210ab1e088
Bartosz Zbytniewki <zbyti@yahoo.com>
2022-02-10 17:05:18 +0100

    atari.crt keypressed fixed and A8 SID Collectioin

--- samples/a8/sound/sid-music-collection/main.pas
+++ samples/a8/sound/sid-music-collection/main.pas
@@ -2,7 +2,7 @@
 
 //-----------------------------------------------------------------------------
 
-uses c64, aplib, crt;
+uses atari, aplib, crt;
 
 //-----------------------------------------------------------------------------
 
@@ -12,47 +12,41 @@ const
   MUSIC_APL_BATMANIA   = $5100;
   MUSIC_APL_CONTAXIA   = $5800;
   MUSIC_APL_DOMINATION = $6000;
-  MUSIC_APL_FALCON     = $6900;
-  MUSIC_APL_FUNCIE     = $7000;
-  MUSIC_APL_CHANCE     = $7900;
-  MUSIC_APL_LOVE       = $8100;
-  MUSIC_APL_LILY       = $8900;
-  MUSIC_APL_PARTY      = $9100;
-  MUSIC_APL_PIZZA      = $9800;
+  MUSIC_APL_FUNCIE     = $6900;
+  MUSIC_APL_CHANCE     = $7200;
+  MUSIC_APL_LOVE       = $7a00;
+  MUSIC_APL_PIZZA      = $8200;
 
-  MUSIC                = $c000;
+  MUSIC                = $9000;
   M_INIT               = MUSIC + $48;
   M_PLAY               = MUSIC + $21;
   M_SPACE              = $1000;
-  M_COUNTER            = 11;
+  M_COUNTER            = 8;
 
 const
   RESET_VECTOR         = $fffc;
-  SID_REG_HEAD         = $d400;
+  SID_REG_HEAD         = $d500;
 
-  RASTER_START         = $e0;
+  RASTER_START         = $10;
 
 //-----------------------------------------------------------------------------
 
 names: array [0..M_COUNTER] of string[15] = (
   'London Demo    ',
-  'Audio Art      ',
   'Batmania II 5  ',
+  'Audio Art      ',
   'Contaxia       ',
-  'Party Tune     ',
   'Domination     ',
-  'Falcon Tn      ',
   'Funcie         ',
   'In Chance      ',
   'Lessons in Love',
-  'Lily was here  ',
   'Peppered Pizza '
 );
 
 zaks: array [0..M_COUNTER] of word = (
-  MUSIC_APL_LONDON, MUSIC_APL_ART, MUSIC_APL_BATMANIA, MUSIC_APL_CONTAXIA, MUSIC_APL_PARTY,
-  MUSIC_APL_DOMINATION, MUSIC_APL_FALCON, MUSIC_APL_FUNCIE, MUSIC_APL_CHANCE, MUSIC_APL_LOVE,
-  MUSIC_APL_LILY, MUSIC_APL_PIZZA
+  MUSIC_APL_LONDON, MUSIC_APL_BATMANIA, MUSIC_APL_ART, MUSIC_APL_CONTAXIA,
+  MUSIC_APL_DOMINATION, MUSIC_APL_FUNCIE, MUSIC_APL_CHANCE, MUSIC_APL_LOVE,
+  MUSIC_APL_PIZZA
 );
 
 //-----------------------------------------------------------------------------
@@ -103,15 +97,16 @@ begin
   repeat
     prepare_new_music;
 
-    write(#$13, 'No.', music_index + 1, ' ', names[music_index]);
+    writeln('No.', music_index + 1, ' ', names[music_index]);
 
     repeat
-      repeat until RasterCounter = RASTER_START;
-      inc(BorderColor);
+      pause;
+      repeat until vcount = RASTER_START;
+      colbk := $0e;
 
       music_play;
       
-      dec(BorderColor);
+      colbk := 0;
     until keypressed;
 
     sid_off;
@@ -124,11 +119,13 @@ end;
 
 begin
   clrscr;
-  
-  writeln(#13);
+  writeln('Expecting SID on $d500');
+  writeln;
   writeln('Composer : Reyn Ouwehand');
   writeln('Player   : Music Assembler');
-  writeln(#13'press space...');
+  writeln;
+  writeln('press space...');
+  writeln;
 
   main_loop;
 
