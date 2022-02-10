d6aaccfa86c9c537a199edfa7d1e184aa5026d87
Tomasz Biela <tebe6502@o2.pl>
2022-02-11 00:43:35 +0100

    C64 : Keypress

---
+++ samples/c64/keypressed.pas
@@ -0,0 +1,27 @@
+uses crt;
+
+var ch: char;
+
+begin
+
+repeat until keypressed;
+ch:=readkey();                              // tutaj odczytuje klawisz
+
+writeln('1');
+
+repeat until keypressed;
+ch:=readkey();                              // tutaj odczytuje klawisz
+
+writeln('2');
+
+repeat until keypressed;
+ch:=readkey();                              // tutaj odczytuje klawisz
+
+writeln('3');
+
+repeat until keypressed;
+ch:=readkey();                              // tutaj odczytuje klawisz
+
+writeln('4');
+
+end.
