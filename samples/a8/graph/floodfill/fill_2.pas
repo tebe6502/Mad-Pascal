ec3966cee4ba8d2557387ff47e4a1185e0ae6fed
Tomasz Biela <tebe6502@gmail.com>
2022-09-04 10:23:32 +0200

    Add files via upload

--- samples/a8/graph/floodfill/fill_2.pas
+++ samples/a8/graph/floodfill/fill_2.pas
@@ -1,4 +1,4 @@
-// 117
+// 94
 
 program gr23;
 
@@ -20,12 +20,12 @@ begin
   lineto (159,50);
   lineto (159,79);
   lineto (0,79);
-  
+
   FloodFillH(80,78,2);
-  
+
  ticks:=GetTickCount - ticks;
  writeln(ticks,' ticks');
- 
+
 
   repeat until keypressed;
 
