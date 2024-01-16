3b61c3f1690a4950a9727020b7bd00b7f863a404
Tomasz Biela <tebe6502@gmail.com>
2024-01-16 12:00:50 +0100

    PETSCII special code

--- base/c64/putchar.asm
+++ base/c64/putchar.asm
@@ -3,11 +3,31 @@
 
   PUT CHAR
 
+  https://sta.c64.org/cbm64pet.html
+  
+  Codes $00-$1F and $80-$9F are control codes. 
+  
 */
 
 .proc	@putchar (.byte a) .reg
 
 chrout	= $ffd2			;kernel character output sub
+
+	tay
+
+	cmp #$20
+	bcc @+
+
+	tya
+	clc			; clear carry for add
+	adc #$FF-$9F		; make m = $FF
+	adc #$9F-$80+1		; carry set if in range n to m
+	jcc skp
+@
+	tya
+	jmp chrout
+
+skp	tya
 
 	cmp #64
 	scc
