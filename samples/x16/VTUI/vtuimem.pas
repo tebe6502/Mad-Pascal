48da79abdf2abc69ec7998b8dcd9fefbbe4da368
MADRAFi <madrafi@gmail.com>
2024-03-06 02:07:06 +0100

    fixed restoring background

--- samples/x16/VTUI/vtuimem.pas
+++ samples/x16/VTUI/vtuimem.pas
@@ -8,19 +8,11 @@ var
 procedure moveMem;
 begin
     vtuiGotoXY(oldX,oldY);
-    vtuiSaveRect(7,7,$0100,$80);
-    vtuiGotoXY(oldX,oldY);
-    // vtuiRestoreRect(7,7,0,$80);
-    vtuiGotoXY(cX,cY);
     vtuiRestoreRect(7,7,$0100,$80);
-
-
-    // vtuiGotoXY(oldX,oldY);
-    // vtuiRestoreRect(7,7,$0100,$80);
-    // vtuiGotoXY(cX,cY);
-    // vtuiSaveRect(7,7,$0100,$80);
-    // vtuiGotoXY(cX,cY);
-    // vtuiRestoreRect(7,7,0,$80);
+    vtuiGotoXY(cX,cY);
+    vtuiSaveRect(7,7,$0100,$80);
+    vtuigotoxy(cX,cY);
+    vtuiRestoreRect(7,7,$0000,$80);
 
     oldX:=cX;
     oldY:=cY;
@@ -34,9 +26,14 @@ begin
 
     vtuigotoxy(cX,cY);
     vtuiSaveRect(7,7,$0000,$80);
+    vtuigotoxy(cX,cY);
+    vtuiFillBox(' ',7,7, BLUE shl 4 + BLUE);
+    vtuigotoxy(cX,cY);
+    vtuiSaveRect(7,7,$0100,$80);
     vtuigotoxy(70,50);
     vtuiRestoreRect(7,7,$0000,$80);
     vtuigotoxy(cX,cY);
+    vtuiRestoreRect(7,7,$0000,$80);
 
     repeat
         key:=#0;
@@ -46,13 +43,13 @@ begin
                 if cX > 0 then Dec(cX);        
             end;
             'D','d': begin
-                if cX < 79 then Inc(cX);
+                if cX < 73 then Inc(cX);
             end;
             'W','w': begin
                 if cY > 0 then Dec(cY);
             end;
             'S', 's': begin
-                if cY < 59 then Inc(cY);
+                if cY < 53 then Inc(cY);
             end;
         end;
         If (key<>#0) and (key<>X16_KEY_ESC) then moveMem;
