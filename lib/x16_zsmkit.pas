6eeb9d01583eb30f109399139c16b3239288e101
MADRAFi <madrafi@gmail.com>
2024-03-08 02:17:05 +0100

    reworked sound unit

--- lib/x16_zsmkit.pas
+++ lib/x16_zsmkit.pas
@@ -26,7 +26,6 @@ const
 
 
 var
-    // zsmRAMBank: byte;
     oldIRQ: Word;
 
 
@@ -39,21 +38,79 @@ procedure zsmInit; assembler;
 * 
 *)
 
-procedure zsmDirectLoad(filename: String; Bank: Byte); assembler;
+procedure zsmSetISR; assembler;
 (*
 * @description:
-* Loads filename music file into bank.
+* This sets up a default interrupt service routine that calls zsm_tick on every interrupt. The existing IRQ handler is called afterwards.
 *
 * 
 * 
 *)
 
-procedure zsmSetMemBank(priorityRAMbank: Byte); assembler;
+procedure zsmClearISR; assembler;
+(*
+* @description:
+* This routine removes the interrupt service routine.
+*
+* 
+* 
+*)
+
+procedure zsmDirectLoad(filename: String; bank: Byte; addr: Word); assembler;
+(*
+* @description:
+* Loads filename music file into bank.
+*
+* @param (String) - Filename to load
+* @param (Byte) - RAM bank to load to
+* @param (Word) - Address in Memory to load to
+* 
+*)
+
+procedure zsmSetMem(priority: Byte; bank: Byte; addr: Word); assembler;
 (*
 * @description:
 * Sets up the song pointers and parses the header based on a ZSM that was previously loaded into RAM. If the song is valid, it marks the priority slot as playable.
 *
+* @param: (Byte) - Sets the priority for the music
+* @param: (Byte) - Switches to chosen bank 1-63 (512kb) or 1-255 (2048kb)
+* @param: (Word) - Address in memory to switch to
 * 
+*)
+
+procedure zsmPlay(priority: Byte); assembler;
+(*
+* @description:
+* Starts playback of a song. If zsm_stop was called, this function continues playback from the point that it was stopped.
+*
+* @param: (Byte) - Priority for playback 0-255
+* 
+*)
+
+procedure zsmStop(priority: Byte); assembler;
+(*
+* @description:
+* Pauses playback of a song. Playback can optionally be resumed from that point later with zsm_play.
+*
+* @param: (Byte) - Priority for playback 0-255
+* 
+*)
+
+procedure zsmRewind(priority: Byte); assembler;
+(*
+* @description:
+* Stops playback of a song (if it is already playing) and resets its pointer to the beginning of the song. Playback can then be started again with zsm_play.
+*
+* @param: (Byte) - Priority for playback 0-255
+* 
+*)
+
+procedure zsmSetAtten(priority, volume: Byte); assembler;
+(*
+* @description:
+* Changes the master volume of a priority slot by setting an attenuation value. A value of $00 implies no attenuation (full volume) and a value of $3F is full mute.
+*
+* @param: (Byte) - Priority for playback 0-255
 * 
 *)
 
@@ -63,78 +120,128 @@ implementation
 
 procedure zsmInit; assembler;
 asm
+    
     lda #1
     jsr zsm_init_engine
 
-    jsr setup_handler
+    // lda IRQVec
+    // sta oldIRQ
+    // lda IRQVec+1
+    // sta oldIRQ+1
 
-    .proc setup_handler
-        lda IRQVec
-        sta oldIRQ
-        lda IRQVec+1
-        sta oldIRQ+1
+    // sei
+    // lda #<irqhandler
+    // sta IRQVec
+    // lda #>irqhandler
+    // sta IRQVec+1
+    // cli
 
-        sei
-        lda #<irqhandler
-        sta IRQVec
-        lda #>irqhandler
-        sta IRQVec+1
-        cli
+    // rts
 
-        rts
-    .endp
-
-    .proc irqhandler
-        lda #35
-        sta VERA_dc_border
-        lda #0
-        jsr zsm_tick
-        jmp (oldIRQ)
-    .endp
+    // .proc irqhandler
+    //     lda #35
+    //     sta VERA_dc_border
+    //     lda #0
+    //     jsr zsm_tick
+    //     jmp (oldIRQ)
+    // .endp
 
 end;
 
-procedure zsmDirectLoad(filename: String; Bank: Byte); assembler;
+procedure zsmSetISR; assembler;
 asm
+    jsr zsmkit_setisr
+end;
+
+procedure zsmClearISR; assembler;
+asm
+    jsr zsmkit_clearisr
+end;
+
+procedure zsmDirectLoad(filename: String; bank: Byte; addr: Word); assembler;
+asm
+        pha
         phx
+        phy
 
-
-
-        lda #2; // logical file number
-        ldx #8; // device number
-        ldy #2; // doing bvload
-        jsr SETLFS
-
-        // copy pointer to zero page
-        lda filename
+        lda #<(adr.filename+1)
         sta r12L
-        lda filename+1
+        lda #>(adr.filename+1)
         sta r12H
 
-        // read first pointed-at byte - string length
-        ldy #0
-        lda (r12L),y
-        
+        lda adr.filename
         // get pointer into x,y registers
         ldx r12L
         ldy r12H
-        
-        // increment past the length
-        inx
-        bne skip
-        iny
-
-    skip:
         jsr SETNAM
 
+        lda #2; // logical file number
+        ldx #8; // device number
+        ldy #2;
+        jsr SETLFS
 
-        lda #Bank; // BVLOAD
-        ldx #0;
-        ldy #0
+        lda bank
+        sta RAMBank
+        lda #0
+        ldx addr
+        ldy addr+1
+
         jsr LOAD
-        plx   
+
+        ply
+        plx
+        pla
 end;
 
+procedure zsmSetMem(priority: Byte; bank: Byte; addr: Word); assembler;
+asm
+    pha
+    phx
+    phy
 
+    lda bank
+    sta RAMBank
+    
+    ldx priority
+    lda addr
+    ldy addr+1
+    jsr zsm_setmem
+
+    ply  
+    plx
+    pla
+end;
+
+procedure zsmPlay(priority: Byte); assembler;
+asm
+    phx
+    ldx priority
+    jsr zsm_play
+    plx
+end;
+
+procedure zsmStop(priority: Byte); assembler;
+asm
+    phx
+    ldx priority
+    jsr zsm_stop
+    plx
+end;
+
+procedure zsmRewind(priority: Byte); assembler;
+asm
+    phx
+    ldx priority
+    jsr zsm_rewind
+    plx
+end;
+
+procedure zsmSetAtten(priority, volume: Byte); assembler;
+asm
+    phx
+    ldx priority
+    lda volume
+    plx
+end;
 
 end.
---
+++ samples/x16/Music/zsmkit.pas
@@ -0,0 +1,16 @@
+uses x16_zsmkit, x16, crt;
+
+
+begin
+  writeln('zsm init');
+  zsmInit;
+  writeln('loading sound...');
+  zsmDirectLoad('music.zsm', 2, $A000);
+  writeln('set ram bank');
+  zsmSetMem(1, 2, $A000);
+  writeln('play?');
+  zsmPlay(1);
+
+  writeln;
+  writeln('playing music in background.');
+end.
