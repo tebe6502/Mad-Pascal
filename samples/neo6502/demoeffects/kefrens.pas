8135334c486b0770337710f0f12c0b315d6bdfd2
Bartek Zbytniewski <zbyti@yahoo.com>
2024-04-01 19:35:16 +0200

    Neo6502: kefrens bars

---
+++ samples/neo6502/demoeffects/kefrens.pas
@@ -0,0 +1,53 @@
+program rainbow;
+
+//------------------------------------------------------------------------------
+
+uses crt, fastmath, neo6502;
+
+//------------------------------------------------------------------------------
+
+const
+    BAR_W_OUTER = 4;
+    BAR_W_INNER = 2;
+    BAR_H = 200;
+    BAR_COL1 = 4;
+    BAR_COL2 = 11;
+
+//------------------------------------------------------------------------------
+
+var
+    i, ii      : byte;
+    sinusTable : array [0..255] of byte absolute $F000;
+
+//------------------------------------------------------------------------------
+
+procedure drawBawr(n :byte; x: word); register;
+var
+    size : word absolute $54;
+    h    : byte absolute $56;
+begin
+    size := BAR_H + n;
+    if size > 239 then h := 239 else h := size;
+
+    NeoSetColor(BAR_COL1);
+    NeoDrawRect(x, n , x + BAR_W_OUTER, h);
+
+    NeoSetColor(BAR_COL2);
+    NeoDrawRect(x + 1, n, x + 1 + BAR_W_INNER, h);
+    NeoDrawLine(x + 2, n, x + 2, h);
+end;
+
+//------------------------------------------------------------------------------
+
+begin
+    FillSinHigh(@sinusTable);
+    repeat
+        NeoWaitForVblank;
+        clrscr;
+
+        for ii := 0 to 59  do begin
+            drawBawr(ii, ii + sinusTable[i + (ii shl 1)]);
+        end;
+        inc(i);
+    until false;
+end.
