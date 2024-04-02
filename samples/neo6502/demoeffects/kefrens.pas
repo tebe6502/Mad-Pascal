8a10dd90b34b2188e220e3a5029fb31010cc99bc
Bartek Zbytniewski <zbyti@yahoo.com>
2024-04-02 10:49:25 +0200

    Neo6502: kefrens bars - refactor

--- samples/neo6502/demoeffects/kefrens.pas
+++ samples/neo6502/demoeffects/kefrens.pas
@@ -1,4 +1,4 @@
-program kefrens_bars;
+program rainbow;
 
 //------------------------------------------------------------------------------
 
@@ -7,8 +7,8 @@ uses crt, fastmath, neo6502;
 //------------------------------------------------------------------------------
 
 const
-    BAR_W_OUTER = 4;
-    BAR_W_INNER = 2;
+    BAR_W_OUTER = 6;
+    BAR_W_INNER = BAR_W_OUTER - 2;
     BAR_H = 200;
     BAR_COL1 = 4;
     BAR_COL2 = 11;
@@ -29,12 +29,11 @@ begin
     size := BAR_H + n;
     if size > 239 then h := 239 else h := size;
 
+    NeoSetSolidFlag(1);
     NeoSetColor(BAR_COL1);
     NeoDrawRect(x, n , x + BAR_W_OUTER, h);
-
     NeoSetColor(BAR_COL2);
     NeoDrawRect(x + 1, n, x + 1 + BAR_W_INNER, h);
-    NeoDrawLine(x + 2, n, x + 2, h);
 end;
 
 //------------------------------------------------------------------------------
@@ -44,8 +43,7 @@ begin
     repeat
         NeoWaitForVblank;
         clrscr;
-
-        for ii := 0 to 59 do
+        for ii := 0 to 59  do
             drawBar(ii, ii + sinusTable[i + (ii shl 1)]);
         inc(i);
     until false;
