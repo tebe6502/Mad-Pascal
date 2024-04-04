a8699830b1f837741663390bf16566bccbc23a67
Tomasz Biela <tebe6502@gmail.com>
2024-04-04 21:27:10 +0200

    Power Packer decompressor

---
+++ lib/pp.pas
@@ -0,0 +1,32 @@
+unit pp;
+(*
+* @type: unit
+* @author: Krzysztof 'Swiety/Zelax' Swiecicki, Tomasz 'Tebe' Biela
+* @name: Power Packer
+*
+* @version: 1.0
+*
+* @description:
+* Power Packer decompressor
+*
+* <https://github.com/lab313ru/powerpacker_src>
+*
+* <https://github.com/retrocoder68/PowerPacker>
+*
+*)
+
+{
+
+unPP
+
+}
+
+interface
+
+	procedure unPP(src, dst: pointer); external 'pp\unpp';
+
+implementation
+
+	{$link pp\unpp.obx}
+
+end.
