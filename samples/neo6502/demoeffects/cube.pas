e9d23616071f1d72e50deba71be690330deccf6b
Bartek Zbytniewski <zbyti@yahoo.com>
2024-08-26 20:50:54 +0200

    Neo6502 - sample/cube refactor

--- samples/neo6502/demoeffects/cube.pas
+++ samples/neo6502/demoeffects/cube.pas
@@ -2,7 +2,7 @@ program wired_cube;
 
 //------------------------------------------------------------------------------
 
-uses crt, neo6502;
+uses crt, neo6502system, neo6502;
 
 //------------------------------------------------------------------------------
 
@@ -49,9 +49,11 @@ begin
         rzp[n,0] := -cs * pts[n,0] + sn * pts[n,1];
         rzp[n,1] :=  sn * pts[n,0] + cs * pts[n,1];
         rzp[n,2] :=  pts[n,2];
-        ryp[n,0] :=  cs * rzp[n,0] +  sn * rzp[n,2];
+        
+        ryp[n,0] :=  cs * rzp[n,0] + sn * rzp[n,2];
         ryp[n,1] :=  rzp[n,1];
-        ryp[n,2] := -sn * rzp[n,0] +  cs * rzp[n,2];
+        ryp[n,2] := -sn * rzp[n,0] + cs * rzp[n,2];
+        
         rxp[n,0] :=  ryp[n,0];
         rxp[n,1] :=  cs * ryp[n,1] - sn * ryp[n,2];
         rxp[n,2] :=  sn * ryp[n,1] + cs * ryp[n,2];
@@ -60,76 +62,78 @@ end;
 
 procedure draw_lines;
 begin
-    X1 := Trunc(ryp[0,0] * sz + cx);
-    Y1 := Trunc(ryp[0,1] * sz + cy);
-    X2 := Trunc(ryp[1,0] * sz + cx);
-    Y2 := Trunc(ryp[1,1] * sz + cy);
+    X1 := Trunc(rxp[0,0] * sz + cx);
+    Y1 := Trunc(rxp[0,1] * sz + cy);
+    X2 := Trunc(rxp[1,0] * sz + cx);
+    Y2 := Trunc(rxp[1,1] * sz + cy);
+
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[1,0] * sz + cx);
-    Y1 := Trunc(ryp[1,1] * sz + cy);
-    X2 := Trunc(ryp[2,0] * sz + cx);
-    Y2 := Trunc(ryp[2,1] * sz + cy);
+    X1 := Trunc(rxp[1,0] * sz + cx);
+    Y1 := Trunc(rxp[1,1] * sz + cy);
+    X2 := Trunc(rxp[2,0] * sz + cx);
+    Y2 := Trunc(rxp[2,1] * sz + cy);
+
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[2,0] * sz + cx);
-    Y1 := Trunc(ryp[2,1] * sz + cy);
-    X2 := Trunc(ryp[3,0] * sz + cx);
-    Y2 := Trunc(ryp[3,1] * sz + cy);
+    X1 := Trunc(rxp[2,0] * sz + cx);
+    Y1 := Trunc(rxp[2,1] * sz + cy);
+    X2 := Trunc(rxp[3,0] * sz + cx);
+    Y2 := Trunc(rxp[3,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[3,0] * sz + cx);
-    Y1 := Trunc(ryp[3,1] * sz + cy);
-    X2 := Trunc(ryp[0,0] * sz + cx);
-    Y2 := Trunc(ryp[0,1] * sz + cy);
+    X1 := Trunc(rxp[3,0] * sz + cx);
+    Y1 := Trunc(rxp[3,1] * sz + cy);
+    X2 := Trunc(rxp[0,0] * sz + cx);
+    Y2 := Trunc(rxp[0,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[4,0] * sz + cx);
-    Y1 := Trunc(ryp[4,1] * sz + cy);
-    X2 := Trunc(ryp[5,0] * sz + cx);
-    Y2 := Trunc(ryp[5,1] * sz + cy);
+    X1 := Trunc(rxp[4,0] * sz + cx);
+    Y1 := Trunc(rxp[4,1] * sz + cy);
+    X2 := Trunc(rxp[5,0] * sz + cx);
+    Y2 := Trunc(rxp[5,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[5,0] * sz + cx);
-    Y1 := Trunc(ryp[5,1] * sz + cy);
-    X2 := Trunc(ryp[6,0] * sz + cx);
-    Y2 := Trunc(ryp[6,1] * sz + cy);
+    X1 := Trunc(rxp[5,0] * sz + cx);
+    Y1 := Trunc(rxp[5,1] * sz + cy);
+    X2 := Trunc(rxp[6,0] * sz + cx);
+    Y2 := Trunc(rxp[6,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[6,0] * sz + cx);
-    Y1 := Trunc(ryp[6,1] * sz + cy);
-    X2 := Trunc(ryp[7,0] * sz + cx);
-    Y2 := Trunc(ryp[7,1] * sz + cy);
+    X1 := Trunc(rxp[6,0] * sz + cx);
+    Y1 := Trunc(rxp[6,1] * sz + cy);
+    X2 := Trunc(rxp[7,0] * sz + cx);
+    Y2 := Trunc(rxp[7,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[7,0] * sz + cx);
-    Y1 := Trunc(ryp[7,1] * sz + cy);
-    X2 := Trunc(ryp[4,0] * sz + cx);
-    Y2 := Trunc(ryp[4,1] * sz + cy);
+    X1 := Trunc(rxp[7,0] * sz + cx);
+    Y1 := Trunc(rxp[7,1] * sz + cy);
+    X2 := Trunc(rxp[4,0] * sz + cx);
+    Y2 := Trunc(rxp[4,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[0,0] * sz + cx);
-    Y1 := Trunc(ryp[0,1] * sz + cy);
-    X2 := Trunc(ryp[4,0] * sz + cx);
-    Y2 := Trunc(ryp[4,1] * sz + cy);
+    X1 := Trunc(rxp[0,0] * sz + cx);
+    Y1 := Trunc(rxp[0,1] * sz + cy);
+    X2 := Trunc(rxp[4,0] * sz + cx);
+    Y2 := Trunc(rxp[4,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[1,0] * sz + cx);
-    Y1 := Trunc(ryp[1,1] * sz + cy);
-    X2 := Trunc(ryp[5,0] * sz + cx);
-    Y2 := Trunc(ryp[5,1] * sz + cy);
+    X1 := Trunc(rxp[1,0] * sz + cx);
+    Y1 := Trunc(rxp[1,1] * sz + cy);
+    X2 := Trunc(rxp[5,0] * sz + cx);
+    Y2 := Trunc(rxp[5,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[2,0] * sz + cx);
-    Y1 := Trunc(ryp[2,1] * sz + cy);
-    X2 := Trunc(ryp[6,0] * sz + cx);
-    Y2 := Trunc(ryp[6,1] * sz + cy);
+    X1 := Trunc(rxp[2,0] * sz + cx);
+    Y1 := Trunc(rxp[2,1] * sz + cy);
+    X2 := Trunc(rxp[6,0] * sz + cx);
+    Y2 := Trunc(rxp[6,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 
-    X1 := Trunc(ryp[3,0] * sz + cx);
-    Y1 := Trunc(ryp[3,1] * sz + cy);
-    X2 := Trunc(ryp[7,0] * sz + cx);
-    Y2 := Trunc(ryp[7,1] * sz + cy);
+    X1 := Trunc(rxp[3,0] * sz + cx);
+    Y1 := Trunc(rxp[3,1] * sz + cy);
+    X2 := Trunc(rxp[7,0] * sz + cx);
+    Y2 := Trunc(rxp[7,1] * sz + cy);
     NeoDrawLine(X1,Y1,X2,Y2);
 end;
 
@@ -144,6 +148,6 @@ begin
 
         rotate_points;
         draw_lines;
-        angle := angle + 0.1;
+        angle := angle + 0.02;
     until false;
 end.
