c34e6731cc18f941340ffb8388edcb4dc6410e29
tebe6502 <tebe6502@o2.pl>
2024-09-13 19:08:40 +0200

    fix ':' -> ';'

--- blibs/xbios.pas
+++ blibs/xbios.pas
@@ -801,7 +801,7 @@ asm {
 };
 end;
 
-procedure xBiosFindEntry(var filename: TString): assembler;
+procedure xBiosFindEntry(var filename: TString); assembler;
 asm {
     txa:pha
     mva #0 xBiosIOresult
@@ -821,7 +821,7 @@ asm {
 };
 end;
 
-procedure xBiosSetBufferSize(size: byte): assembler;
+procedure xBiosSetBufferSize(size: byte); assembler;
 asm {
     txa:pha
     lda #$ff
