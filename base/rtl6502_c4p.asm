3e6f1838d599dc9fa7c064e0ad2a6e3bd0dc4f89
tebe6502 <tebe6502@gmail.com>
2025-01-14 12:50:00 +0100

    optimization

--- base/rtl6502_c4p.asm
+++ base/rtl6502_c4p.asm
@@ -17,27 +17,6 @@
 
 */
 
-//MAXSIZE = 4
-//EOL	= $0D
-//@buf	= $0800		; lo addr = 0 !!!
-//fracpart = eax
-
-; -----------------------------------------------------------------------
-
-.enum	e@file
-	eof = 1, open, assign
-.ende
-
-.struct	s@file
-pfname	.word		; pointer to string with filename
-record	.word		; record size
-chanel	.byte		; channel *$10
-status	.byte		; status bit 0..7
-buffer	.word		; load/write buffer
-nrecord	.word		; number of records for load/write
-numread	.word		; pointer to variable, length of loaded data
-.ends
-
 ; -----------------------------------------------------------------------
 
 	icl 'runtime\macros.asm'
