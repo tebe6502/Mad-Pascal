01b8c2c60908a0fa5a46d959a9181d06809fd110
tebe6502 <tebe6502@gmail.com>
2025-01-14 12:50:00 +0100

    optimization

--- base/rtl_default.asm
+++ base/rtl_default.asm
@@ -1,4 +1,22 @@
-	MAXSIZE: = 4
+MAXSIZE = 4
+
+; -----------------------------------------------------------------------
+
+.enum	e@file
+	eof = 1, open, assign
+.ende
+
+.struct	s@file
+pfname	.word		; pointer to string with filename
+record	.word		; record size
+chanel	.byte		; channel *$10
+status	.byte		; status bit 0..7
+buffer	.word		; load/write buffer
+nrecord	.word		; number of records for load/write
+numread	.word		; pointer to variable, length of loaded data
+.ends
+
+; -----------------------------------------------------------------------
 
 	icl 'runtime\int.asm'		; @INT, @INT_SHORT
 
