4792a3653a93bcd3891230ddedd78a727c6d5b8d
Peter Dell <peter.dell@web.de>
2025-03-10 20:28:23 +0100

    Add Assert method to TNumber tests

--- src/Test-MP.pas
+++ src/Test-MP.pas
@@ -1,6 +1,29 @@
 program test;
+uses Crt;
 
+
+procedure Assert(b: Boolean; s: String);
 begin
+ if (b) then
+ begin
+   Write('OK: ');
+ end
+ else
+ begin
+   Write('ERROR:');
+ end;
+ Writeln(s);
+end;
 
-  Writeln('Test completed.');
+var i: Integer;
+begin
+  i:=1;
+  Writeln(i);
+  Inc(i);
+  Writeln(i);
+  Assert(i=2, 'I=2');
+  Assert(1+1=2, '1+1=2');
+  Writeln('Test completed. Press any key');
+  repeat
+  until KeyPressed;
 end.
