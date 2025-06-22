9bd7ff2785618317d6c1090edf330985c9d6c42e
Peter Dell <peter.dell@web.de>
2025-06-22 21:12:49 +0200

    Fix and format BlowFish
    Disable FPC-specific parts

--- samples/a8/math/blowfish/fpc/blowfish_test.pas
+++ samples/a8/math/blowfish/fpc/blowfish_test.pas
@@ -1,69 +1,79 @@
-uses crt, classes, blowfish;
+program blowfish_test;
+
+(* This program is for FPC, not for MP.
+
+uses
+  crt,
+  Classes,
+  BlowFish;
 
 var
-	myencryptedstring, mydecryptedstring: string[32];
+  myencryptedstring, mydecryptedstring: String[32];
 
-	i: byte;
+  i: Byte;
 
 
-function EncryptString(aString:string):string;
-var Key:string;
-    EncrytpStream:TBlowFishEncryptStream;
-    StringStream:TStringStream;
-    EncryptedString:string;
+  function EncryptString(aString: String): String;
+  var
+    Key: String;
+    EncrytpStream: TBlowFishEncryptStream;
+    StringStream: TStringStream;
+    EncryptedString: String;
+  begin
+    Key := 'your_secret_encryption_key';
+    StringStream := TStringStream.Create('');
+    EncrytpStream := TBlowFishEncryptStream.Create(Key, StringStream);
+    EncrytpStream.WriteAnsiString(aString);
+    EncrytpStream.Free;
+    EncryptedString := StringStream.DataString;
+    StringStream.Free;
+    EncryptString := EncryptedString;
+  end;
+
+
+  function DecryptString(aString: String): String;
+  var
+    Key: String;
+    DecrytpStream: TBlowFishDeCryptStream;
+    StringStream: TStringStream;
+    DecryptedString: String;
+  begin
+    Key := 'your_secret_encryption_key';
+    StringStream := TStringStream.Create(aString);
+    DecrytpStream := TBlowFishDeCryptStream.Create(Key, StringStream);
+    DecryptedString := DecrytpStream.ReadAnsiString;
+    DecrytpStream.Free;
+    StringStream.Free;
+    DecryptString := DecryptedString;
+  end;
+
+
+  // -------------------------------------------------------------
+
+
+  *)
+
 begin
-  Key := 'your_secret_encryption_key';
-  StringStream := TStringStream.Create('');
-  EncrytpStream := TBlowFishEncryptStream.Create(Key,StringStream);
-  EncrytpStream.WriteAnsiString(aString);
-  EncrytpStream.Free;
-  EncryptedString := StringStream.DataString;
-  StringStream.Free;
-  EncryptString := EncryptedString;
-end;
+(*
+  myencryptedstring := EncryptString('Atari Power With Price');        // returns unreadable text
+  mydecryptedstring := DecryptString(myencryptedstring);  // returns 'Hello'
 
 
-function DecryptString(aString:string):string;
-var Key:string;
-    DecrytpStream:TBlowFishDeCryptStream;
-    StringStream:TStringStream;
-    DecryptedString:string;
-begin
-  Key := 'your_secret_encryption_key';
-  StringStream := TStringStream.Create(aString);
-  DecrytpStream := TBlowFishDeCryptStream.Create(Key,StringStream);
-  DecryptedString := DecrytpStream.ReadAnsiString;
-  DecrytpStream.Free;
-  StringStream.Free;
-  DecryptString := DecryptedString;
-end;
+  writeln(length(myencryptedstring));
 
+  for i := 1 to length(myencryptedstring) do
+    Write(hexStr(Byte(myencryptedstring[i]), 2), ',');
 
-// -------------------------------------------------------------
+  WriteLn;
 
+  //WriteLn(myencryptedstring);
 
+  writeln(mydecryptedstring);
 
-begin
+  repeat
+  until keypressed;
 
-myencryptedstring:=EncryptString('Atari Power With Price');      	// returns unreadable text
-mydecryptedstring:=DecryptString(myencryptedstring);	// returns 'Hello'
-
-
-writeln(length(myencryptedstring));
-
-for i:=1 to length(myencryptedstring) do
- write(hexStr(byte(myencryptedstring[i]),2),',');
-
-writeln;
-
-//writeln(myencryptedstring);
-
-writeln(mydecryptedstring);
-
-repeat until keypressed;
-
+*)
 end.
 
-
-// 6A,B3,3A,0A,41,4A,16,9E,17,DD,B8,61,69,D4,B0,C4,
-// Hello
+// 6A,B3,3A,0A,41,4A,16,9E,17,DD,B8,61,69,D4,B0,C4,// Hello
