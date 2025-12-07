3cfac388063ccf484c614332b85a837d78e836bd
Peter Dell <peter.dell@web.de>
2025-12-07 22:39:36 +0100

    samples: c64, Fix Play_SID.pas
    Correct code, texts and song.

--- samples/c64/sid/Play_SID.pas
+++ samples/c64/sid/Play_SID.pas
@@ -1,96 +1,69 @@
 { ==================================
-  Demo program - SID player (c64)
-  Created: 12/5/2022
-  Song:    What It Takes, by Jimmy Nielsen (Shogun)
-  URL:     https://csdb.dk/sid/?id=57014
-   ================================== }
+  SID player (C64)
+  ================================== }
 program PlaySID;
-uses Commodore64;
+
+uses crt, sysutils;
 
 var
-  CIA_Interrupt : byte absolute $DC0D;
-  ScreenControl : byte absolute $D011;
-  RasterLine    : byte absolute $D012;
-  InterruptFlag : byte absolute $D019;
-  InterruptMask : byte absolute $D01A;
+  CIAInterrupt     : byte absolute $DC0D;
+  ScreenControl    : byte absolute $D011;
+  RasterLine       : byte absolute $D012;
+  InterruptFlag    : byte absolute $D019;
+  InterruptMask    : byte absolute $D01A;
+  BorderColor      : byte absolute $D020;
+  BackgroundColor0 : byte absolute $D021;
 
   IRQ: word absolute $314;
-  SID: array of byte = ({$BIN2CSV Test.sid});
+  SID: array of byte = [{$BIN2CSV 'Play_SID.sid'}];
   
-  Src, Dst, Count: word ZeroPage;
-  
-  procedure CopyR;
-  begin
-    asm
-          LDA SID + $0A     ; get address of Init
-          STA Dst+1
-          LDA SID + $0B
-          STA Dst
-          LDX Count+1  ; the last byte must be moved first
-          CLC          ; start at the final pages of FROM and TO
-          TXA
-          ADC Src+1
-          STA Src+1
-          CLC
-          TXA
-          ADC Dst+1
-          STA Dst+1
-          INX          ; allows the use of BNE after the DEX below
-          LDY Count
-          BEQ next
-          DEY          ; move bytes on the last page first
-          BEQ loop2
- loop1:   LDA (Src),Y
-          STA (Dst),Y
-          DEY
-          BNE loop1
- loop2:   LDA (Src),Y ; handle Y = 0 separately
-          STA (Dst),Y
- next:    DEY
-          DEC Src+1   ; move the next page (if any)
-          DEC Dst+1
-          DEX
-          BNE loop1
-    end; 
-  end;
+  Src  : Word;
+  Dst  : Word;
+  Size : Word;
+  InitAddress : Word;
+  PlayAddress : Word;
    
 begin
-  Src   := @SID + $7E;
-  //Dst   := @SID + $7C;
-  Count := SID.Length - $7E;
-  CopyR;
-  
-  CIA_Interrupt := $7F;  // stops IRQs from CIA
-  asm 
-          LDA SID + $0A     ; get address of Init
-          STA init + 2
-          LDA SID + $0B
-          STA init + 1
-          LDA SID + $0C     ; get address of Play
-          STA play + 2
-          LDA SID + $0D
-          STA play + 1
-          
-	        LDA #0            ; start with first song
-  init:   JSR $0000         ; Init SID player
+
+  BorderColor:=0;
+  BackgroundColor0:=0;
+  WriteLn(#147#5'Doublebass (v2)');
+  WriteLn(#159'Aidan Crouzet-Pascal (acrouzet)');
+  WriteLn(#30'2021 Genesis Project');
+  WriteLn(#156'https://demozoo.org/music/330333');
+  WriteLn;
+
+  Src   := Word(@SID + $7E);
+  Dst   := SID[$7C]+256*SID[$7D];
+  Size  := Length(SID) - $7E;
+  Move(Pointer(src), Pointer(Dst), Size);
+  WriteLn(#151'Copied $',HexStr(Size,4),' bytes from $',HexStr(Src,4),' to $',HexStr(Dst,4),'.');
+
+  InitAddress   := 256*SID[$0A]+SID[$0B];
+  PlayAddress   := 256*SID[$0C]+SID[$0D];
+  WriteLn(#151'Init address $',HexStr(InitAddress,4),', play address $',HexStr(PlayAddress,4),'.');
+  CIAInterrupt := $7F;  // stops IRQs from CIA
+  asm
+          MWA InitAddress init+1  ; get address of Init
+          MWA PlayAddress play+1  ; get address of Play
+
+	  LDA #0                  ; start with the first song
+  init:   JSR $ffff               ; initialize the SID player
           SEI
-          LDA #1
-          STA InterruptMask ; enable raster interrupts
-          LDA #$64
-          STA RasterLine    ; raise IRQ in that line
+          MVA #1 InterruptMask    ; enable raster interrupts
+          MVA #$64 RasterLine     ; raise IRQ in that line
           LDA ScreenControl
-          AND #$7F          ; Clear bit 7
-          STA ScreenControl ; 9th bit of RasterLine
-          LDA #<IRQ_Handler ; Hijack IRQ vector
-          STA IRQ
-          LDA #>IRQ_Handler
-          STA IRQ + 1
+          AND #$7F                ; Clear bit 7
+          STA ScreenControl       ; 9th bit of RasterLine
+          MWA #IRQ_Handler IRQ    ; Hijack IRQ vector
           CLI
           RTS
+
           
   IRQ_Handler:          
-          INC InterruptFlag ; ack the interrupt
-  play:   JSR $0000         ; Play
-          JMP $EA31  
+          INC InterruptFlag       ; ack the interrupt
+
+  play:   JSR $ffff               ; Play
+          JMP $EA31
   end; 
 end.
