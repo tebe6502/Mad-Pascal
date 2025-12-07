4f5836d4b97f6a15dd05879570f33445ce8327ff
Peter Dell <peter.dell@web.de>
2025-12-07 17:46:38 +0100

    src: Update makefile
    Always build origin and src version

--- src/Makefile.bat
+++ src/Makefile.bat
@@ -16,24 +16,21 @@ set PATH=%WUDSN_TOOLS_FOLDER%\PAS\FPC.jac;%WUDSN_TOOLS_FOLDER%\ASM\MADS\bin\wind
 call :normalize_path %~dp0..
 set MP_FOLDER=%RETVAL%
 set MP_SRC_FOLDER=%MP_FOLDER%\src
-set MP_PAS=%MP_SRC_FOLDER%\mp.pas
+set MP_PAS_FILE=mp.pas
 set MP_BIN_FOLDER=%MP_FOLDER%\bin\windows_x86_64
-set MP_EXE=%MP_BIN_FOLDER%\mp.exe
-
-
-rem set REFERENCE_MP_FOLDER=C:\jac\system\Atari800\Programming\Repositories\Mad-Pascal.174
-rem set REFERENCE_MP_EXE=%REFERENCE_MP_FOLDER%\bin\windows\mp.exe
+set MP_EXE_FILE=mp.exe
+set MP_EXE=%MP_BIN_FOLDER%\%MP_EXE_FILE%
 
 set REFERENCE_MP_FOLDER=C:\jac\system\Atari800\Programming\Repositories\Mad-Pascal
-set REFERENCE_MP_EXE=%REFERENCE_MP_FOLDER%\bin\windows_x86_64\origin\mp.exe
+set REFERENCE_MP_SRC_FOLDER=%REFERENCE_MP_FOLDER%\origin
+set REFERENCE_MP_BIN_FOLDER=%MP_FOLDER%\bin\windows_x86_64\origin
+set REFERENCE_MP_EXE=%REFERENCE_MP_BIN_FOLDER%\mp.exe
 
 set TEST_PAS=%MP_SRC_FOLDER%\TestUnits.pas
 rem set TEST_EXE=%MP_SRC_FOLDER%\TestUnits.exe
-
 set MP_TESTS_FOLDER=%MP_SRC_FOLDER%\tests
 
 cd /d %MP_SRC_FOLDER%
-
 
 if not "%TEST_EXE%"=="" (
   if exist "%TEST_EXE%" del "%TEST_EXE%"
@@ -42,44 +39,30 @@ if not "%TEST_EXE%"=="" (
 )
 
 
+call :make_exe %MP_SRC_FOLDER% %MP_BIN_FOLDER%
+if errorlevel 1 goto :eof
 
-if not "%MP_EXE%"=="" (
-  echo INFO: Compiling %MP_PAS% to %MP_EXE%.
-  if exist "%MP_EXE%" del "%MP_EXE%"
-  call fpc.bat %MP_PAS%
-  if errorlevel 1 goto :eof
-  if not exist %MP_BIN_FOLDER% mkdir "%MP_BIN_FOLDER%"
-  copy "mp.exe" "%MP_EXE%"
-  if errorlevel 1 goto :eof
-
-  rem DEL does not set the errorlevel!
-  del "mp.exe"
-  if exist "mp.exe" (
-    echo WARNING: Cannot delete %CD%\mp.exe
-  )
-)
-
-set FPP_MASTER=C:\Users\JAC\Downloads\fpprofiler-master.zip\fpprofiler-master
-set PATH=%FPP_MASTER%\fpp;%FPP_MASTER%\fppview;%PATH%
-
+call :make_exe %REFERENCE_MP_SRC_FOLDER% %REFERENCE_MP_BIN_FOLDER%
+if errorlevel 1 goto :eof
 
 set PAS_FOLDER=samples\a8\math\AES-Rijndael
 set PAS_FILE=rijndael-test
 
+rem Default regression test sample.
 set PAS_FOLDER=samples\tests\tests-debug
 set PAS_FILE=debug
 
 cd %MP_FOLDER%\%PAS_FOLDER%
 %REFERENCE_MP_EXE% -ipath %REFERENCE_MP_FOLDER%\lib -ipath %REFERENCE_MP_FOLDER%\blibs %PAS_FILE%.pas
-if exist %PAS_FILE%-Reference.a65         del %PAS_FILE%-Reference.a65
+if errorlevel 1 goto :eof
+
+if exist %PAS_FILE%-Reference.a65 del %PAS_FILE%-Reference.a65
 ren %PAS_FILE%.a65 %PAS_FILE%-Reference.a65
 
 %MP_EXE% -ipath %MP_FOLDER%\lib -ipath %MP_FOLDER%\blibs %PAS_FILE%.pas
+if errorlevel 1 goto :eof
 
 start .
-
-rem Regression test with standard MP.
-rem call %MP_FOLDER%\projects\MakeMadPascal.bat
 
 goto :eof
 
@@ -87,3 +70,40 @@ goto :eof
 :normalize_path
   set retval=%~f1
   exit /b
+
+rem call :make_exe <SRC_FOLDER> <BIN_FOLDER>
+rem Uses: %MP_PAS_FILE%, %MP_EXE_FILE%
+:make_exe
+setlocal
+echo on
+set SRC_FOLDER=%1
+set BIN_FOLDER=%2
+set PAS=%SRC_FOLDER%\%MP_PAS_FILE%
+set EXE_FILE=%MP_EXE_FILE%
+set EXE=%BIN_FOLDER%\%MP_EXE_FILE%
+
+cd /d %SRC_FOLDER%
+
+if not "%EXE_FILE%"=="" (
+  echo INFO: Compiling %PAS% to %EXE%.
+  if exist "%EXE%" del "%EXE%"
+  call fpc.bat %PAS%
+  if errorlevel 1 goto :eof
+  if not exist %BIN_FOLDER% mkdir "%BIN_FOLDER%"
+  copy "%EXE_FILE%" "%EXE%"
+  if errorlevel 1 goto :eof
+
+  rem DEL does not set the errorlevel!
+  del "%EXE_FILE%"
+  if exist "%EXE_FILE%" (
+    echo WARNING: Cannot delete %CD%\%MP_EXE_FILE%
+  )
+
+  rem Output version
+  %EXE%
+  exit /b 0
+)
+endlocal
+goto :eof
+
+
