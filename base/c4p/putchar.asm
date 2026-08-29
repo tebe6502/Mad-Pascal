48299d5f3750c0862de9a2bcb7f21c39b5c08174
tebe6502 <tebe6502@o2.pl>
2026-08-29 08:19:25 +0200

    issue #280 fixed

--- base/c4p/putchar.asm
+++ base/c4p/putchar.asm
@@ -5,11 +5,13 @@
 
 */
 
+; https://github.com/tebe6502/Mad-Pascal/issues/280 
+
 .proc	@putchar (.byte a) .reg
 
 chrout	= $ffd2                ;kernel character output sub
 
-	cmp #64
+	cmp #64+1
 	scc
 	eor #%00100000
 
