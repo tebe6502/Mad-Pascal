af0a9bc2ca82dad4a51340f9d4e4e45eaea8f6b7
tebe6502 <tebe6502@o2.pl>
2026-09-17 23:17:03 +0200

    update

--- base/common/float16_frac.asm
+++ base/common/float16_frac.asm
@@ -26,3 +26,74 @@ A	= :EAX+2
 	jmp @F16_SUB
 
 .endp
+
+
+
+/*
+ 
+function FracHalfBitwise(a: float16): word;
+var
+  h: word absolute a;
+  sign: Word;
+  p: byte;
+  exp: byte;
+  width: byte;
+  mant, fracMask, fracBits: Word;
+begin
+  sign := h and $8000;
+  exp  := (h shr 10) and $1F;
+  mant := h and $3FF;
+ 
+  if exp = 31 then           // Inf / NaN
+  begin
+    Result := sign or $7E00; // qNaN
+    Exit;
+  end;
+
+  if exp = 0 then             // zero lub subnormalna: już < 1
+  begin
+    Result := h;
+    Exit;
+  end;
+
+  if exp < 15 then                // |x| < 1 -> cała wartość to ułamek
+  begin
+    Result := h;
+    Exit;
+  end;
+
+  dec(exp, 15);                 // wykładnik nieobciążony
+
+  if exp >= 10 then               // liczba całkowita -> ułamek = 0
+  begin
+    Result := sign;             // +0 lub -0
+    Exit;
+  end;
+
+  // 0 <= e < 10: ułamek siedzi w dolnych (10-e) bitach mantysy
+  width    := 10 - exp;
+  fracMask := (Word(1) shl width) - 1;
+  fracBits := mant and fracMask;
+
+  if fracBits = 0 then
+  begin
+    Result := sign;              // liczba była całkowita
+    Exit;
+  end;
+
+  // szukamy pozycji najwyższego ustawionego bitu (BSR na "width" bitach)
+  p := width - 1;
+  while (fracBits and (Word(1) shl p)) = 0 do
+    Dec(p);
+
+  exp  := exp - 10 + p + 15;                    // nowy wykładnik (obciążony)
+  mant := (fracBits - (Word(1) shl p)) shl (10 - p);
+  
+  mant:=mant and $3FF;
+
+  result := sign or (exp shl 10) or mant;
+
+end;
+
+
+*/
